1c83581a94ea0afd248bd67412b3c5db599e7e7b
jeffkmaina <jeffrey.k.maina@gmail.com>
2022-05-19 09:43:29 +0000

    splitting shell to only have 5 functions

--- shell.c
+++ shell.c
@@ -7,30 +7,26 @@
  *
  * Return: 0 success. Non-zero otherwise
  */
+
 int shell(list_t *env_list, char *shell_name)
 {
-	char *input, *full_name;
+	char *input, *full_name, *error_message[4];
 	list_t *input_list;
 	char **input_array;
 	int built_ret, exec_ret;
-	char *error_message[4];
 
 	error_message_init(error_message, shell_name, NULL);
-
 	while (1)
 	{
 		input = get_input();
 		if (input == NULL)
 			return (0);
-
 		input_list = split_string(input, " "); /* check input */
 		if (input_list == NULL || input[0] == '\n')
 		{
 			free(input);
 			continue;
 		}
-
-		/* check if input is a built-in command */
 		built_ret = get_built(input_list, shell_name, env_list);
 		if (built_ret != -1)
 		{
@@ -40,9 +36,7 @@ int shell(list_t *env_list, char *shell_name)
 			else
 				return (built_ret);
 		}
-
-		/* check if 1st string is a valid command */
-		error_message[1] = input_list->name;
+		error_message[1] = input_list->name;/* checkifisvalidcommand */
 		full_name = get_full_name(input_list->name, env_list);
 		if (full_name == NULL)
 		{
@@ -51,9 +45,7 @@ int shell(list_t *env_list, char *shell_name)
 			free_input(input, input_list, NULL);
 			continue;
 		}
-
-		/* change input_list to input_array */
-		input_array = list_to_array(input_list);
+		input_array = list_to_array(input_list);/* changelisttoarray */
 		if (input_array == NULL)
 		{
 			error_message[2] = "malloc failed";
@@ -62,20 +54,16 @@ int shell(list_t *env_list, char *shell_name)
 			free_input(input, input_list, NULL);
 			continue;
 		}
-
-		/* execute command */
-		exec_ret = execute(input_array, full_name, shell_name);
+		exec_ret = execute(input_array, full_name, shell_name);/* execute command */
 		if (exec_ret < 0)
 		{
 			free_input(input, input_list, input_array);
 			free(full_name);
 			return (-exec_ret);
 		}
-
 		free(full_name);
 		free_input(input, input_list, input_array);
 	}
-
 	return (0);
 }
 
@@ -103,7 +91,7 @@ char *get_input(void)
 
 	/* replace tabs with spaces */
 	str_rep(buffer, '\t', ' ');
-	
+
 	return (_strlen(buffer) == 1 ? buffer : _strtok(buffer, "\n"));
 }
 
@@ -161,60 +149,4 @@ void free_input(char *input, list_t *input_list, char **input_array)
 
 	if (input_array)
 		free_array(input_array);
-}
-
-/**
- * execute - executes a given input command
- * @input_array: array of strings containing input command
- * @command: name of command to execute
- * @shell_name: name of shell program
- *
- * Return: 0 success. Otherwise negative integer
- */
-int execute(char **input_array, char *command, char *shell_name)
-{
-	pid_t child_pid;
-	int status;
-	char *error_message[4];
-
-	error_message_init(error_message, shell_name, command);
-
-	child_pid = fork();
-	if (child_pid == -1)
-	{
-		error_message[2] = "fork error";
-		print_error(error_message);
-		return (-1);
-	}
-	else if (child_pid == 0) /* execute command */
-	{
-		if (execve(command, input_array, NULL) == -1)
-		{
-			error_message[2] = "execve error";
-			print_error(error_message);
-			return (-2);
-		}
-	}
-	else
-	{
-		wait(&status);
-
-		if (WIFEXITED(status))
-			return (WEXITSTATUS(status));
-	}
-
-	return (0);
-}
-
-/** error_message_init - initialize error_message array with given values
- * @error_message: error message array
- * @shell_name: 1st value
- * @command: 2nd value
- */
-void error_message_init(char **error_message, char *shell_name, char *command)
-{
-	error_message[0] = shell_name;
-	error_message[1] = command;
-	error_message[2] = NULL;
-	error_message[3] = NULL;
 }
